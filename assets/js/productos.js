a61bc9530f8df8fdd0e30a4d86cf888d2c863e77
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-10-10 12:12:50 -0500

    fecht listo5

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -2,12 +2,12 @@
 
 
 async function obtenerData() {
-  let respuesta = await fetch('data.json');
-  // let respuesta = await fetch('https://api.jsonbin.io/v3/b/6344358a65b57a31e691a42a');
+  // let respuesta = await fetch('data.json');
+  let respuesta = await fetch('https://api.jsonbin.io/v3/b/6344358a65b57a31e691a42a');
   const data = await respuesta.json();
-  // const prod = await data.record;
+  const prod = await data.record;
   // console.log(data);
-  return data
+  return prod
 }
 
 
